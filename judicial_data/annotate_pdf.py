cab81bd48c6487c1eb601826e46151d2b3f961e0
Paul Butler <paulgb@gmail.com>
2019-08-31 10:40:50 -0400

    better pdf parsing

--- judicial_data/annotate_pdf.py
+++ judicial_data/annotate_pdf.py
@@ -1,5 +1,5 @@
 from os import makedirs, path
-from typing import List
+from typing import List, Tuple
 
 import pdfrw
 from pdf_annotate import Appearance, Location, PdfAnnotator
@@ -8,12 +8,14 @@ from judicial_data import parse_pdf
 
 RED = (1, 0, 0)
 BLUE = (0, 0, 1)
-
+GREY = (0.3, 0.3, 0.3)
+GREEN = (0, 1, 0)
+PURPLE = (1, 0, 1)
 
 class Annotator:
     def __init__(self, filename: str):
         self.filename = filename
-        self.annotations = list()
+        self.annotations: List[Tuple[str, Location, Appearance]] = list()
         self.get_page_dimensions()
 
     def get_page_dimensions(self):
@@ -22,20 +24,30 @@ class Annotator:
         self.page_width = float(pdf.pages[0].MediaBox[2])
         self.page_height = float(pdf.pages[0].MediaBox[3])
 
-    def annotate_cols(self, cols: List[float]):
-        for page in range(self.num_pages):
-            for col in cols:
-                loc = Location(points=[[col, 0],
-                                       [col, self.page_height]], page=page)
-                appearance = Appearance(stroke_color=RED,
-                                        stroke_width=2)
-                self.annotations.append(('line', loc, appearance))
+    def annotate_cols(self, cols: List[float], page: int):
+        for col in cols:
+            loc = Location(points=[[col, 0],
+                                    [col, self.page_height]], page=page)
+            appearance = Appearance(stroke_color=RED,
+                                    stroke_width=2)
+            self.annotations.append(('line', loc, appearance))
 
-    def annotate_text(self, text: 'parse_pdf.Text', note=None):
+    def annotate_text(self, text: 'parse_pdf.Text', color=BLUE):
         loc = Location(
             x1=text.x_min, y1=self.page_height-text.y_max,
             x2=text.x_max, y2=self.page_height-text.y_min, page=text.page)
-        appearance = Appearance(stroke_color=BLUE, stroke_width=1)
+        appearance = Appearance(stroke_color=color, stroke_width=1)
+        self.annotations.append(('square', loc, appearance))
+
+    def annotate_group(self, texts: List['parse_pdf.Text'], color=BLUE):
+        x1 = min(t.x_min for t in texts)
+        x2 = max(t.x_max for t in texts)
+        y1 = min(t.y_min for t in texts)
+        y2 = min(t.y_max for t in texts)
+        loc = Location(
+            x1=x1, y1=self.page_height-y2,
+            x2=x2, y2=self.page_height-y1, page=texts[0].page)
+        appearance = Appearance(stroke_color=color, stroke_width=1)
         self.annotations.append(('square', loc, appearance))
 
     def produce_debug_pdf(self):
--- judicial_data/parse_pdf.py
+++ judicial_data/parse_pdf.py
@@ -2,13 +2,14 @@ import re
 import subprocess
 import tempfile
 from collections import namedtuple
-from datetime import datetime
+from datetime import date, datetime
 from os import path
-from typing import List
+from typing import Iterator, List, Tuple, Optional, Dict
 from xml.etree import ElementTree
-from judicial_data import annotate_pdf
+import pandas as pd
 from more_itertools import peekable
 
+from judicial_data import annotate_pdf
 
 FORMATS = ['%b %d, %Y', '%B %d, %Y']
 PAGE_ELEM = r'.//{http://www.w3.org/1999/xhtml}page'
@@ -19,28 +20,32 @@ Text = namedtuple('Text', ['page', 'x_min', 'x_max', 'y_min', 'y_max', 'text'])
 
 
 class PDFParser:
+    texts: List[Text]
+    col_map: Optional[Dict[str, float]] = None
+
     def __init__(self, filename: str):
         self.annotator = annotate_pdf.Annotator(filename)
-
         self.filename = filename
         doc = self.read_pdf_as_html(filename)
         self.extract_text_parts(doc)
-        self.col_map = self.create_col_map()
+        self.parse_doc()
 
     @property
-    def parts(self):
-        text_iter = peekable(iter(self.texts))
+    def parts(self) -> Iterator[List[Text]]:
+        text_iter = peekable(self.texts)
+        page = 0
         while text_iter:
             text = next(text_iter)
-            line = [text]
+            line: List[Text] = [text]
             y = text.y_max
+            page = text.page
 
-            while text_iter.peek().y_min < y:
+            while text_iter and text_iter.peek().page == page and text_iter.peek().y_min < y:
                 line.append(next(text_iter))
-            yield line
+            yield sorted(line, key=lambda x: x.x_min)
 
     @staticmethod
-    def parse_date(d: str) -> datetime:
+    def parse_date(d: str) -> Optional[date]:
         for fmt in FORMATS:
             try:
                 return datetime.strptime(d, fmt).date()
@@ -51,7 +56,8 @@ class PDFParser:
     @staticmethod
     def read_pdf_as_html(pdf_file):
         with tempfile.TemporaryDirectory() as temp_dir:
-            result_file = path.join(temp_dir, 'result.html')
+            #result_file = path.join(temp_dir, 'result.html')
+            result_file = 'temp.html'
 
             subprocess.check_call([
                 'pdftotext',
@@ -63,7 +69,7 @@ class PDFParser:
 
             return ElementTree.parse(result_file)
 
-    def extract_text_parts(self, doc) -> List[Text]:
+    def extract_text_parts(self, doc):
         root = doc.getroot()
         pages = root.findall(PAGE_ELEM)
 
@@ -88,18 +94,88 @@ class PDFParser:
         self.texts = list(
             sorted(texts, key=(lambda x: (x.page, x.y_min, x.x_min))))
 
-    def create_col_map(self):
-        col_mappings = {}
+    def get_column_from_x(self, x):
+        return next(c for c, x_ in reversed(list(self.col_map.items())) if x_ <= x)
+
+    def parse_doc(self):
+        self.rows = []
+        stop_collecting = False
         for group in self.parts:
-            print(group)
-            if group[0].text in {'Circuit', 'Court'}:
-                for t in group:
-                    self.annotator.annotate_text(t)
+            alltext = ' '.join(a.text for a in group)
 
-                    name = t.text
-                    name = re.sub(r'\s+', ' ', name)
-                    loc = t.x_min - 10
-                    col_mappings[name] = loc
+            if alltext.startswith('Vacancies in the Federal Judiciary'):
+                self.annotator.annotate_group(group, annotate_pdf.GREY)
+            elif alltext.startswith('Total Vacancies:'):
+                self.annotator.annotate_group(group, annotate_pdf.GREEN)
+                if 'Total Nominees Pending' in alltext:
+                    total_vac = int(alltext.split()[-2])
+                    total_noms = total_vac = int(alltext.split()[-1])
+                else:
+                    total_vac = int(alltext.split(' ')[-1])
+            elif alltext.startswith('Total Nominees Pending:'):
+                self.annotator.annotate_group(group, annotate_pdf.GREEN)
+                total_noms = int(alltext.split(' ')[-1])
+            elif group[0].text in ['Circuit', 'Court']:
+                self.create_col_map(group)
+            elif self.parse_date(alltext):
+                self.annotator.annotate_group(group, annotate_pdf.GREEN)
+                date = self.parse_date(alltext)
+            elif group[0].text.endswith(' Circuit') or alltext == 'International Trade':
+                self.annotator.annotate_group(group, annotate_pdf.GREEN)
+                circuit = group[0].text
+                court = None
+            elif alltext == 'VACANCIES':
+                stop_collecting = True # Hit end of document, don’t treat as table
+            elif alltext.startswith('Federal Judicial Vacancies') or alltext.startswith('http:'):
+                self.annotator.annotate_group(group, annotate_pdf.GREY)
+            elif not stop_collecting:
+                if not self.col_map:
+                    print('no col mappings')
+                    continue
+                r = {
+                    self.get_column_from_x(t.x_min): t.text
+                    for t in group
+                }
 
-                self.annotator.annotate_cols(col_mappings.values())
-                return col_mappings
+                for c in ['Vacancy Date', 'Nomination']:
+                    if c in r:
+                        r[c] = pd.Timestamp(r[c]).date()
+
+                if len(r) == 1 and 'Court' in r:
+                    court = r['Court']
+                    self.annotator.annotate_group(group, annotate_pdf.PURPLE)
+                    continue
+
+                if 'Nominee' not in r:
+                    r['Nominee'] = None
+                if 'Nomination' not in r:
+                    r['Nomination'] = None
+
+                if 'Circuit' not in r:
+                    r['Circuit'] = court
+                else:
+                    court = r['Circuit']
+                r['CT'] = circuit
+                
+                r['date'] = date
+                self.annotator.annotate_group(group, annotate_pdf.BLUE)
+                self.rows.append(r)
+
+    def create_col_map(self, group):
+        col_mappings = {}
+        last_max = None
+
+        for t in group:
+            self.annotator.annotate_text(t)
+
+            name = t.text
+            name = re.sub(r'\s+', ' ', name)
+            if last_max is None:
+                loc = t.x_min
+            else:
+                loc = (last_max + t.x_min) / 2
+            last_max = t.x_max
+            col_mappings[name] = loc - 10
+
+        self.annotator.annotate_cols(col_mappings.values(), group[0].page)
+        self.col_map = col_mappings
